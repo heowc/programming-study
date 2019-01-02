b2b2ce6bd33a1f009a56dccd00e14677d07b750a
heowc <heowc1992@gmail.com>
2019-01-02 22:19:07 +0900

    [FIX] gh-pages 브랜치 동기화 스크립트 수정

--- sync-gh-pages.sh
+++ sync-gh-pages.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-echo 'change branch & checkout: gh-pages'
-git checkout -b gh-pages origin/gh-pages
+function existBranch() {
+    if [[ -z `git branch | grep $1` ]]; then
+        echo 'true'
+    else
+        echo 'false'
+    fi
+}
 
-echo 'sync master branch from submodule of gh-pages branch'
-git submodule update --remote --merge
+if [[ `existBranch 'gh-pages'` -eq 'true' ]]; then
+    echo 'change branch & pull : gh-pages'
+    git checkout gh-pages && git pull
+else
+    echo 'change branch & checkout: gh-pages'
+#    git checkout -b gh-pages origin/gh-pages
+fi
 
-echo 'push origin/gh-pages'
-git add -A && git commit -m "repo 갱신" && git push
-
-echo 'change branch: master'
-git checkout -b master
+#echo 'sync master branch from submodule of gh-pages branch'
+#git submodule update --remote --merge
+#
+#echo 'push origin/gh-pages'
+#git add -A && git commit -m "sync repo" && git push
+#
+#echo 'change branch: master'
+#git checkout master
